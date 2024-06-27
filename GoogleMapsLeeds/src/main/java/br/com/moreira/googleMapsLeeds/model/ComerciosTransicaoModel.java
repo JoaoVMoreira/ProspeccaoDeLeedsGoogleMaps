6420c1096860448607ba2fcc78ceb69f04a96a3d
JoaoVMoreira <jvittormoreira@outlook.pt>
2024-06-27 00:27:56 -0300

    configurando banco de dados e modelando tabelas

---
+++ GoogleMapsLeeds/src/main/java/br/com/moreira/googleMapsLeeds/model/ComerciosTransicaoModel.java
@@ -0,0 +1,108 @@
+package br.com.moreira.googleMapsLeeds.model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "db_comercios_transicao")
+public class ComerciosTransicaoModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    @Column(name = "c_nome")
+    private String nome;
+    @Column(name = "c_segmento")
+    private String segmento;
+    @Column(name = "c_contato", unique = true)
+    private String contato;
+    @Column(name = "c_site", unique = true)
+    private String site;
+    @Column(name = "c_ctt_realizado")
+    private Boolean cttRealizado;
+    @Column(name = "c_cidade")
+    private String cidade;
+    @Column(name = "c_possui_wpp")
+    private Boolean possuiWpp;
+
+    //Construtores
+    public ComerciosTransicaoModel(){
+
+    }
+
+    public ComerciosTransicaoModel(String id, String nome, String segmento, String contato, String site, Boolean cttRealizado, String cidade, Boolean possuiWpp){
+        this.id = id;
+        this.nome = nome;
+        this.segmento = segmento;
+        this.contato = contato;
+        this.site = site;
+        this.cttRealizado = cttRealizado;
+        this.cidade = cidade;
+        this.possuiWpp = possuiWpp;
+    }
+
+    //Getters e Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSegmento(){
+        return segmento;
+    }
+
+    public void setSegmento(String segmento){
+        this.segmento = segmento;
+    }
+
+    public String getContato(){
+        return contato;
+    }
+
+    public void setContato(String contato){
+        this.contato = contato;
+    }
+
+    public String getSite(){
+        return site;
+    }
+
+    public void setSite(String site){
+        this.site = site;
+    }
+
+    public Boolean getCttRealizado(){
+        return cttRealizado;
+    }
+
+    public void setCttRealizado(Boolean cttRealizado){
+        this.cttRealizado = cttRealizado;
+    }
+
+    public String getCidade(){
+        return cidade;
+    }
+
+    public void setCidade(String cidade){
+        this.cidade = cidade;
+    }
+
+    public Boolean getPossuiWpp(){
+        return possuiWpp;
+    }
+
+    public void setPossuiWpp(Boolean possuiWpp){
+        this.possuiWpp = possuiWpp;
+    }
+
+}
